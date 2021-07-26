d4dad2661dbfbb53ffb8ffddd57f97ee4a3ee28a
Kyle Mayer <x@kylemayer.dev>
2021-07-25 23:19:25 -0700

    get by id test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,20 +29,15 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
+  it('gets an order by id', async () => {
+    const order = await Order.insert({ quantity: 10 });
 
-
-
-
-
-  // it('gets an order by id', async () => {
-  //   const order = await Order.insert({ quantity: 10 });
-
-  //   return request(app)
-  //     .get(`/api/v1/orders/${order.id}`)
-  //     .then((res) => {
-  //       expect(res.body).toEqual(order);
-  //     });
-  // });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
+  });
 
   it('updates an existing order by id', async () => {
     const order = await Order.insert({ quantity: 10 });
