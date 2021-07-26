a66121a298032b5bdbe3184b8e5bbf35b5108a64
Kyle Mayer <x@kylemayer.dev>
2021-07-25 23:24:37 -0700

    get all orders test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,6 +29,16 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
+  it('gets all orders', async () => {
+    const order = await Order.insert({ quantity: 10 });
+
+    return request(app)
+      .get('/api/v1/orders')
+      .then((res) => {
+        expect(res.body).toEqual([order]);
+      });
+  });
+
   it('gets an order by id', async () => {
     const order = await Order.insert({ quantity: 10 });
 
